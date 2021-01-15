9f51bf1980926e36452350c04c148989018eaeba
shahnewaz601 <shahnewaz601@gmail.com>
2021-01-16 05:01:35 +0600

    simple update

--- string.js
+++ string.js
@@ -1,4 +1,6 @@
 var bangladesh = "I love Bangladesh";
 
 // console.log(bangladesh.toLowerCase());
-console.log(bangladesh.indexOf('gla'));
+// console.log(bangladesh.indexOf('gla'));
+// console.log(bangladesh.split('love')); //It can delete word//
+console.log(bangladesh.split(' ')); //It can divided the word//
