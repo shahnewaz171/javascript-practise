673d31839d4daa6a074bf1dbcf06dd9b9194e4ab
shahnewaz601 <shahnewaz601@gmail.com>
2021-01-16 05:26:18 +0600

    simple update

---
+++ index.js
@@ -0,0 +1,18 @@
+//====//
+// var number1 = 25;
+// var number2 = 15.5;
+
+// console.log(number1 + number2);
+
+//====//
+// var number1 = 25;
+// var number2 = '15.5';
+// number2 = parseFloat('15.5'); //Parsing---It can change the variable type as like numerical to string, float to integer, string to integer etc...//
+// console.log(number1 + number2);
+
+//==Specific output Digit ==//
+var number1 = 0.1;
+var number2 = 0.2;
+var total = number1 + number2;
+total  = total.toFixed(5); //Specific output Digit//
+console.log(total);
--- string.js
+++ string.js
@@ -1,6 +1,7 @@
 var bangladesh = "I love Bangladesh";
+var para = 'I am a javascript developer';
 
 // console.log(bangladesh.toLowerCase());
 // console.log(bangladesh.indexOf('gla'));
 // console.log(bangladesh.split('love')); //It can delete word//
-console.log(bangladesh.split(' ')); //It can divided the word//
+// console.log(bangladesh.split(' ')); //It can divided the word//
