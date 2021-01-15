a4be897aabb787db3f230ab41d5dc125817e416d
shahnewaz601 <shahnewaz601@gmail.com>
2021-01-16 05:36:18 +0600

    simple update

--- index.js
+++ index.js
@@ -11,8 +11,18 @@
 // console.log(number1 + number2);
 
 //==Specific output Digit ==//
-var number1 = 0.1;
-var number2 = 0.2;
-var total = number1 + number2;
-total  = total.toFixed(5); //Specific output Digit//
-console.log(total);
+// var number1 = 0.1;
+// var number2 = 0.2;
+// var total = number1 + number2;
+// total  = total.toFixed(5); //Specific output Digit//
+// console.log(total);
+
+// ==== //
+var price1 = 22;
+var price2 = 20;
+// var newprice1 = price1 + 1; //Increment 
+// price2 = price2 + 1; //Another way Increment 
+// price1++; //Another way Increment 1
+price2--;
+console.log(price2);
+
