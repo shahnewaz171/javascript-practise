aa9da5227385a709daf6172ede07b4fbc73b471b
shahnewaz601 <shahnewaz601@gmail.com>
2021-01-17 02:50:42 +0600

    simple update

--- math-absolute-round.js
+++ math-absolute-round.js
@@ -1,12 +1,55 @@
 // absolute
-var number = 5;
-var absoluteNumber = Math.abs(number); //Always gives a positive output
+// var number = 5;
+// var absoluteNumber = Math.abs(number); //Always gives a positive output
 
-console.log(absoluteNumber);
+// console.log(absoluteNumber);
 
 
 // ====//
-var digit = 5.5;
-var result = Math.round(digit); 
+// var digit = 5.5;
+// var result = Math.round(digit); 
 
-console.log(result);
+// console.log(result);
+
+
+// ====//
+// var digit = 5.09999;
+// var result = Math.floor(digit); 
+
+// console.log(result);
+
+
+// ====//
+// var digit = 5.09999;
+// var result = Math.ceil(digit); 
+
+// console.log(result);
+
+var id = 172;
+if(id == 171){
+    console.log('Win');
+}
+else if(id >171){
+    console.log('Waiting');
+}
+else{
+    console.log('Failed');
+}
+// ==Randomly==//
+// var digit = 15.00000000;
+// var result = Math.random(digit); 
+
+// console.log(result);
+
+
+// ====//
+// var digit = Math.random() * 100;
+// var result = Math.round(digit); 
+
+// console.log(result);
+
+
+
+//===Time==//
+var date = new Date('1996-08-31');
+console.log(date);
