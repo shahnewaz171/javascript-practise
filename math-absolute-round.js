38e3cffbb9e28eb20940ce6c525b15105d1a8ede
shahnewaz601 <shahnewaz601@gmail.com>
2021-01-16 06:00:15 +0600

    add round function

---
+++ math-absolute-round.js
@@ -0,0 +1,12 @@
+// absolute
+var number = 5;
+var absoluteNumber = Math.abs(number); //Always gives a positive output
+
+console.log(absoluteNumber);
+
+
+// ====//
+var digit = 5.5;
+var result = Math.round(digit); 
+
+console.log(result);
