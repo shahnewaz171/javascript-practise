e2b0f3924ca70e1fe1e86d8bdee1293dcb070c04
shahnewaz601 <shahnewaz601@gmail.com>
2021-01-17 18:46:35 +0600

    some function added

---
+++ function.js
@@ -0,0 +1,12 @@
+function name(){
+    console.log('Shahnewaz');
+    console.log('Khondokar...Koi tmi');
+}
+
+name();
+
+var date = 14;
+var place = 'Dhaka';
+name();
+var meetPlace = 'Dhanmondhi';
+name();
